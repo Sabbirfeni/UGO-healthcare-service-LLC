cd6d06a42ef69f8526d261f1b8773d1a78fb8a4d
Sabbirfeni <contact.sabbirbhuiyan@gmail.com>
2022-04-27 22:14:39 +0600

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 // ********************************
-// Sidebar js
+// Sidebar js start
 // ********************************
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
@@ -8,3 +8,29 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidepanel").style.width = "0";
   }
+// ********************************
+// Sidebar js end
+// ********************************
+
+
+
+
+
+// ********************************
+// Side form js start
+// ********************************
+
+
+function openContact() {
+  document.getElementById("sideContact").style.width = "350px";
+}
+function closeContact() {
+  document.getElementById("sideContact").style.width = "0";
+}
+
+
+// ********************************
+// Side form js end
+// ********************************
+
+
